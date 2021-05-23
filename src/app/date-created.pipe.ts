f3a5707e01b7b2f24b6d6fee28cafc8f6e186141
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-05-23 23:43:20 +0300

    Add styling to not found message

--- src/app/date-created.pipe.ts
+++ src/app/date-created.pipe.ts
@@ -4,33 +4,43 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateCreated',
 })
 export class DateCreatedPipe implements PipeTransform {
-  transform(value: number): any {
-    if (value) {
-      const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
-      if (seconds < 29) {
-        return 'Just now';
-      }
-      const intervals = {
-        year: 31536000,
-        month: 2592000,
-        week: 604800,
-        day: 86400,
-        hour: 3600,
-        minute: 60,
-        second: 1,
-      };
-      let counter;
-      for (const i in intervals) {
-        counter = Math.floor(seconds);
-        if (counter > 0) {
-          if (counter === 1) {
-            return counter + ' ' + i + ' ago';
-          } else {
-            return counter + ' ' + i + 's ago';
-          }
-        }
-      }
-    }
-    return value;
+  transform(value: any): number {
+    let today:any=new Date();
+  
+    var dateDifference=Math.abs(value-today);
+    var dateSeconds=dateDifference*0.001;
+    const seconds=86400;
+    var daysPassed= Math.floor(dateSeconds/seconds);
+
+    return daysPassed;
   }
+  // transform(value: number): any {
+  //   if (value) {
+  //     const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+  //     if (seconds < 29) {
+  //       return 'Just now';
+  //     }
+  //     const intervals = {
+  //       year: 31536000,
+  //       month: 2592000,
+  //       week: 604800,
+  //       day: 86400,
+  //       hour: 3600,
+  //       minute: 60,
+  //       second: 1,
+  //     };
+  //     let counter;
+  //     for (const i in intervals) {
+  //       counter = Math.floor(seconds);
+  //       if (counter > 0) {
+  //         if (counter === 1) {
+  //           return counter + ' ' + i + ' ago';
+  //         } else {
+  //           return counter + ' ' + i + 's ago';
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return value;
+  // }
 }
